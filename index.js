d20eb6eeacde4384fa95ed19ec61ad75d01ac665
Basir <basir.jafarzadeh@gmail.com>
2017-02-05 21:07:15 -0500

    V1

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 app.get('/', function(req, res) {
+  res.clearCookie('username');
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/chat', function(req, res) {
+  if (req.cookies.username) {
+    res.sendFile(__dirname + '/chat.html');
+  } else {
+    if (req.query.username) {
+      res.cookie('username', req.query.username, { maxAge: 900000, httpOnly: false });
+      res.sendFile(__dirname + '/chat.html');
+    } else {
+      res.redirect("/");
+    }
+  }
 });
 
 io.on('connection', function(socket) {
   console.log('user connected.');
-  socket.on('chat message', function(msg) {
-    console.log('user sent msg:' + msg);
-    io.emit('chat message', msg);
+
+  socket.on('ehlo', function(data) {
+    io.emit('chat message', { username: data, msg: 'entered the room.' });
+  });
+  socket.on('chat message', function(data) {
+    console.log('user sent msg:' + data.username);
+    io.emit('chat message', data);
   });
 });
 
